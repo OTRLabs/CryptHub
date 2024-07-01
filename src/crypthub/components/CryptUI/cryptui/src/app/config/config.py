963b82d61f9f765f173a0a7da519be29f67c817f
cam <cammclain@skiff.com>
2024-07-01 13:13:11 -0500

    config shit

--- src/crypthub/components/CryptUI/cryptui/src/app/config/config.py
+++ src/crypthub/components/CryptUI/cryptui/src/app/config/config.py
@@ -7,9 +7,7 @@ from typing import Any
 from litestar import Litestar
 from litestar.config import Config
 
-
 def get_settings() -> dict[str, Any]:
-    
     CRYPT_UI_HOST=os.environ.get("CRYPT_UI_HOST", "localhost")
     CRYPT_UI_PORT=int(os.environ.get("CRYPT_UI_PORT", 8000))
     CRYPT_UI_BASE_ROUTE=os.environ.get("CRYPT_UI_BASE_ROUTE", "/")
