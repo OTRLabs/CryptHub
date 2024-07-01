b1b9ecfd135eb03c28f2d877b4091fdaea0bdc65
cam <cammclain@skiff.com>
2024-07-01 05:22:49 -0500

    changes to asgi stuff. forgot LOL

--- src/crypthub/components/CryptUI/cryptui/src/app/asgi.py
+++ src/crypthub/components/CryptUI/cryptui/src/app/asgi.py
@@ -2,8 +2,27 @@
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from litestar import Litestar
-    
+    from litestar.contrib.jinja import JinjaTemplateEngine
+    from litestar.template.config import TemplateConfig
     
 def create_app() -> Litestar:
     """Create CryptUI Application"""
     
+    from litestar import Litestar
+    
+    
+    settings
+    
+    from pathlib import Path
+
+
+
+    app = Litestar(
+        route_handlers=[],
+        template_config=TemplateConfig(
+            directory=Path("templates"),
+            engine=JinjaTemplateEngine,
+        )
+        
+    return app 
+)
